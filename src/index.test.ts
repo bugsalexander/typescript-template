92337138b269a63d4ece6b80cd42361230d15a5e
meebs <alex@takayamasan.com>
2020-12-13 18:17:41 -0500

    update example files

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,6 @@
-// sample function
-function f(a: number, b: number): number {
-  return a + b;
-}
+import { add1 } from "src";
 
-// sample tests
-it("adds properly", () => {
-  expect(f(1, 2)).toEqual(3);
-  expect(f(-1, 2)).toEqual(1);
-  expect(f(0, 2)).toEqual(2);
-  expect(f(5, 5)).toEqual(10);
+describe("add1 adds one", () => {
+  expect(add1(1)).toBe(2);
+  expect(add1(-1)).toBe(0);
 });
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * adds one to a number
+ * @param n number to add to
+ */
+export function add1(n: number): number {
+  return n + 1;
+}
